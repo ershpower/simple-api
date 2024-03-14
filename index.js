89ec67610f153dfad411377408e2dc1a2174801d
INNO\VDErshov <VDErshov@inno.tech>
2024-03-14 20:27:19 +0400

    stage-2

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     const todoField = document.querySelector('.todo-field');
+    const postsField = document.querySelector('.posts-field');
     const select = document.getElementById('select');
 
-    const showLoadingNotification = () => {
-        todoField.innerText = 'Loading...'
+
+    // TODO start
+    const showLoadingNotification = (node) => {
+        node.innerText = 'Loading...'
     }
 
-    const clearTodoFieldChildren = () => {
-        todoField.innerHTML = '';
+    const clearTodoFieldChildren = (node) => {
+        node.innerHTML = '';
     }
-
     const createLikeNode = () => {
         const like = document.createElement('div');
         like.classList.add('like');
@@ -23,11 +25,11 @@ window.addEventListener('DOMContentLoaded', () => {
     const createTodosList = async (limit = 10) => {
 
         try {
-            showLoadingNotification();
+            showLoadingNotification(todoField);
             const response = await fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
             const todos = await response.json();
             if (todos) {
-                clearTodoFieldChildren();
+                clearTodoFieldChildren(todoField);
                 todos.forEach((todo) => {
                     const card = document.createElement('div');
                     card.classList.add('card');
@@ -63,16 +65,81 @@ window.addEventListener('DOMContentLoaded', () => {
             console.log(e);
         }
 
-
     }
 
     createTodosList();
-
-
     select.addEventListener('change', (e) => {
         const value = e.target.value;
         clearTodoFieldChildren();
         createTodosList(value)
     })
 
+    // TODO end
+
+    // POSTS start
+
+    const postForm = document.getElementById('create-post-form');
+    const createPostsList = async () => {
+        try {
+            showLoadingNotification(postsField);
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+            const posts = await response.json();
+            if (posts) {
+                clearTodoFieldChildren(postsField);
+                posts.forEach((post) => {
+                    const card = document.createElement('div');
+                    card.classList.add('card');
+
+                    const cardBody = document.createElement('div');
+                    card.classList.add('card-body');
+
+                    const cardTitle = document.createElement('h5');
+                    cardTitle.classList.add('card-title');
+                    cardTitle.innerText = post.title;
+
+                    const cardText = document.createElement('p');
+                    cardText.classList.add('card-text');
+                    cardText.innerText = post.body;
+
+                    cardBody.appendChild(cardTitle);
+                    cardBody.appendChild(cardText);
+
+                    card.appendChild(cardBody);
+
+                    postsField.appendChild(card);
+                })
+            }
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    createPostsList()
+
+    postForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const formData = new FormData(postForm);
+
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: "POST",
+                body: formData,
+            })
+            if (response.status === 201) {
+                const successNotificationNode = document.createElement('div');
+                successNotificationNode.innerText = 'Success!';
+                postForm.appendChild(successNotificationNode);
+                setTimeout(() => {
+                    postForm.removeChild(successNotificationNode);
+                }, 1500)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+
+    })
+
+
+    // POSTS end
 })
